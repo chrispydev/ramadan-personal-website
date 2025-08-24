628f67da2f8034cda591aedd08c67432414b0f76
chrispydev <christianowusu44@gmail.com>
2025-08-24 16:11:15 +0000

    feat(file name): navbar mobile responsive design

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,16 @@
+'use client'
+import React, { useState, FC } from 'react';
 import Logo from './logo';
 import NavLink from './nav-link';
 
-const navbar = () => {
+
+const Navbar: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <header className="flex justify-center items-center space-x-4 shadow-md p-4">
+    <header className="flex lg:justify-center justify-between items-center space-x-4 shadow-md p-4  relative ">
       <Logo />
-      <ul className="flex justify-center items-center space-x-4">
+      <ul className="hidden lg:flex justify-center items-center space-x-4">
         <li>
           <NavLink href="/" linkName="Meet Ramadan" />
         </li>
@@ -20,9 +25,30 @@ const navbar = () => {
         </li>
         <NavLink href="/" linkName="Contact Us" />
       </ul>
+      <button onClick={() => setIsOpen(!isOpen)} className='lg:hidden block cursor-pointer'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 lg:hidden">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 6.75h16.5m-16.5 6.75h16.5" />
+        </svg>
+      </button>
+      {isOpen && <>
+        <ul className="lg:hidden flex flex-col justify-center items-start px-4 py-4 space-y-5 absolute z-50 top-full md:w-[20%] right-0 bg-white shadow-md">
+          <li className='hover:ml-4 transition-all duration-500'>
+            <NavLink href="/" linkName="Initiative" />
+          </li>
+          <li className='hover:ml-4 transition-all duration-500'>
+            <NavLink href="/" linkName="Events" />
+          </li>
+          <li className='hover:ml-4 transition-all duration-500'>
+            <NavLink href="/" linkName="News" />
+          </li>
+          <li className='hover:ml-4 transition-all duration-500'>
+            <NavLink href="/" linkName="Contact Us" />
+          </li>
+        </ul>
+      </>}
     </header>
   )
 }
 
-export default navbar
+export default Navbar
 
