0b7b64320b061fc705f7f4af8093ff8747fe27e3
chrispydev <christianowusu44@gmail.com>
2025-08-25 13:54:25 +0000

    feat(file name): header mobile responsive design

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -16,26 +16,24 @@ const header = () => {
         pagination={{ clickable: true }}
         loop={true}
       >
-        <SwiperSlide className="slider-bg-1 background text-white ">
+        <SwiperSlide className="slider-bg-1 background  ">
           <article className="h-[450px] relative  w-full">
-            <div className="absolute top-20 left-5 shadow-md max-w-[40%] mt-10 bg-white text-black py-4 px-2 ml-4">
-              <h1 className="text-4xl font-bold text-center mt-10">
+            <div className="absolute top-40 md:top-20 left-0 md:left-5 shadow-md max-w-full md:max-w-[40%] mt-10 md:bg-white text-white md:text-black py-2 md:py-4 px-1 md:px-2 ml-1 md:ml-4">
+              <h1 className="text-3xl md:text-4xl font-bold text-center mt-10">
                 Welcome to My Portfolio
               </h1>
-              <p className="text-center mt-4 text-lg">
-                Discover my projects, skills, and experiences.
-              </p>
+              <p className="text-center mt-4 text-sm md:text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-
           </article>
         </SwiperSlide>
         <SwiperSlide className="slider-bg-2 background text-white ">
           <article className="h-[450px] relative  w-full">
-            <div className="absolute top-20 left-5 shadow-md max-w-[40%] mt-10 bg-white text-black py-4 px-2 ml-4">
-              <h1 className="text-4xl font-bold text-center mt-10">
+            <div className="absolute top-40 md:top-20 left-0 md:left-5 shadow-md max-w-full md:max-w-[40%] mt-10 md:bg-white text-white md:text-black py-2 md:py-4 px-1 md:px-2 ml-1 md:ml-4">
+              <h1 className="text-3xl md:text-4xl font-bold text-center mt-10">
                 Welcome to My Portfolio
               </h1>
-              <p className="text-center mt-4 text-lg">
+              <p className="text-center mt-4 text-sm md:text-lg">
                 Discover my projects, skills, and experiences.
               </p>
             </div>
@@ -44,11 +42,11 @@ const header = () => {
         </SwiperSlide>
         <SwiperSlide className="slider-bg-3 background text-white ">
           <article className="h-[450px] relative  w-full">
-            <div className="absolute top-20 left-5 shadow-md max-w-[40%] mt-10 bg-white text-black py-4 px-2 ml-4">
-              <h1 className="text-4xl font-bold text-center mt-10">
+            <div className="absolute top-40 md:top-20 left-0 md:left-5 shadow-md max-w-full md:max-w-[40%] mt-10 md:bg-white text-white md:text-black py-2 md:py-4 px-1 md:px-2 ml-1 md:ml-4">
+              <h1 className="text-3xl md:text-4xl font-bold text-center mt-10">
                 Welcome to My Portfolio
               </h1>
-              <p className="text-center mt-4 text-lg">
+              <p className="text-center mt-4 text-sm md:text-lg">
                 Discover my projects, skills, and experiences.
               </p>
             </div>
