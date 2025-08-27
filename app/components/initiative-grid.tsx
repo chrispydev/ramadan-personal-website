6e0d900583827dad8cd083fe347c66fc7b039397
chrispydev <christianowusu44@gmail.com>
2025-08-27 14:11:45 +0000

    feat(file name): initiative gallery responsive design

--- app/components/initiative-grid.tsx
+++ app/components/initiative-grid.tsx
@@ -3,15 +3,15 @@ import InitiativeGallery from './initiative-gallery.tsx'
 
 const InitiativeGrid = () => {
   return (
-    <section className="mt-9 mx-13">
+    <section className="mt-9 md:mx-13 mx-5">
       <article className="flex flex-col items-start">
         <h3 className="text-xl text-gray-500 uppercase tracking-[10px] font-extralight">Initiative</h3>
-        <h2 className="text-4xl font-black capitalize">Mohammed Adamu Ramadan <br /> On the Issue.</h2>
+        <h2 className="mdtext-4xl text-2xl font-black capitalize">Mohammed Adamu Ramadan <br /> On the Issue.</h2>
         <HorizontalLine />
-        <article className="grid grid-cols-3 gap-2">
-        <InitiativeGallery />
-        <InitiativeGallery />
-        <InitiativeGallery />
+        <article className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 md:gap-6 lg:gap-2">
+          <InitiativeGallery />
+          <InitiativeGallery />
+          <InitiativeGallery />
         </article>
       </article>
     </section>
