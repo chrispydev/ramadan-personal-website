fa1b8f5e45ba77f8aa81c4011e6b856915dccdc8
chrispydev <christianowusu44@gmail.com>
2025-08-27 13:59:41 +0000

    feat(file name): about responsive design

--- app/components/about-us.tsx
+++ app/components/about-us.tsx
@@ -3,13 +3,13 @@ import HorizontalLine from "./horizontal-line"
 
 const AboutUs = () => {
   return (
-    <section className="flex flex-col md:flex-row justify-center items-start gap-10 md:mb-4 mx-4">
-      <Image className="lg:w-full h-[500px] lg:h-[560px] object-contain" src="/about-us.jpg" alt="About Us" width={500} height={400} />
+    <section className="flex flex-col md:flex-row justify-center items-start md:gap-10 md:pt-19 pt-0 mb-4 mx-4">
+      <Image className="lg:w-full h-[500px] lg:h-[540px] object-contain -mb-4 md:mb-0" src="/about-us.jpg" alt="About Us" width={500} height={400} />
       <div className="flex flex-col justify-start items-start text-left space-y-2 leading-10">
-        <h5 className="text-2xl md:text-3xl font-bold text-center text-slate-500">
+        <h5 className="text-2xl md:text-3xl font-bold text-center text-gray-700">
           About Us
         </h5>
-        <h2 className="text-lg md:text-2xl font-black uppercase "><span className="text-secondary">Meet</span> Mohammed <br />Adamu Ramadan </h2>
+        <h2 className="text-2xl font-black uppercase "><span className="text-secondary">Meet</span> Mohammed Adamu Ramadan </h2>
         <HorizontalLine />
         <p>Adenta A Town located at the Minicipal Assembly</p>
         <button className="bg-secondary text-white py-2 px-6 rounded">Meet Ramadan</button>
