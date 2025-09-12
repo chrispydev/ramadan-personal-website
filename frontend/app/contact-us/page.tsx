9eae22080d4761f49ba044dbf28c770c365c470b
chrispydev <christianowusu44@gmail.com>
2025-09-12 21:48:31 +0000

    feat(file name): contact page fully working

--- frontend/app/contact-us/page.tsx
+++ frontend/app/contact-us/page.tsx
@@ -1,14 +1,93 @@
-import HorizontalLine from "../components/horizontal-line";
+'use client'
 import { FaRegPaperPlane } from "react-icons/fa";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { FaPhone } from "react-icons/fa6";
 import { FaEnvelope } from "react-icons/fa";
+import { useState, HTMLInputTypeAttribute } from 'react'
+import axios from "axios";
 
-export default async function ContactUs() {
+export default function ContactUs() {
+
+  const [fullName, setFullName] = useState<HTMLInputTypeAttribute>();
+  const [phoneNumber, setPhoneNumber] = useState<HTMLInputTypeAttribute>();
+  const [email, setEmail] = useState<HTMLInputTypeAttribute>();
+  const [subject, setSubject] = useState<HTMLInputTypeAttribute>();
+  const [message, setMessage] = useState<HTMLInputTypeAttribute>();
+
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: false },
+  });
+
+  const handleOnSubmiting = (code: number) => {
+    if (code == 200) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: true },
+      });
+      setFullName('');
+      setEmail('');
+      setPhoneNumber('');
+      setMessage('');
+      setSubject('');
+      setTimeout(() => {
+        setStatus({
+          submitted: false,
+          submitting: false,
+          info: { error: false, msg: false },
+        });
+      }, 2000);
+    } else {
+      setStatus({
+        submitted: false,
+        submitting: false,
+        info: { error: true, msg: false },
+      });
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setStatus({
+      submitted: false,
+      submitting: true,
+      info: { error: false, msg: false },
+    });
+
+    const body = { 'Subject': subject, 'PhoneNumber': phoneNumber, 'message': message }
+
+    axios
+      .post(process.env.NEXT_PUBLIC_FORMSPREE_URL, {
+        data: { fullName: fullName, email: email, message: body },
+      })
+      .then((_response) => {
+        handleOnSubmiting(_response.status);
+      });
+  };
 
 
   return (
     <section className="pt-10 w-[90%] mx-auto grid grid-cols-2">
+      {status.info.error && (
+        <div
+          className='bg-red-100 border border-red-400 text-secondary px-4 py-3 rounded relative'
+          role='alert'
+        >
+          <strong className='font-bold'>Error</strong>:
+          <span className='block sm:inline'>Message not sent????</span>
+        </div>
+      )}
+
+      {status.info.msg && (
+        <div
+          className='bg-red-100 border border-dColor text-dColor px-4 py-3 rounded relative'
+          role='alert'
+        >
+          <span className='block sm:inline'>Message sent 👍</span>
+        </div>
+      )}
       <article>
         <div>
           <h4 className="text-2xl uppercase mb-1 font-bold">Contact Details</h4>
@@ -55,6 +134,25 @@ export default async function ContactUs() {
           <h4 className="text-2xl uppercase mb-1 font-bold">get in touch</h4>
           <div className="w-20 h-2 bg-secondary" />
         </div>
+        <form className="my-8 flex flex-col gap-4">
+          <div className="space-x-8 grid md:grid-cols-2 grid-cols-1 gap-4">
+            <input type="text" placeholder="Full Name" className="py-2 px-4 w-full outline-secondary border-1 border-grayText" onChange={(e) => setFullName(e.target.value)} value={fullName} />            <input type="tel" placeholder="Phone Number" className="py-2 w-full px-4 outline-secondary border-1 border-grayText" onChange={(e) => setPhoneNumber(e.target.value)} value={phoneNumber} />
+          </div>
+          <div>
+            <input type="emal" placeholder="Email Address" className="w-full py-2 px-4 outline-secondary border-1 border-grayText" onChange={(e) => setEmail(e.target.value)} value={email} />
+          </div>
+          <div>
+            <input type="text" placeholder="Subject" className="w-full py-2 px-4 outline-secondary border-1 border-grayText" onChange={(e) => setSubject(e.target.value)} value={subject} />
+          </div>
+          <div>
+            <textarea placeholder="Leave us a message..." rows={5} cols={20} className="w-full py-2 px-4 outline-secondary border-1 border-grayText"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            ></textarea>
+          </div>
+          <button type='submit' className='bg-secondary py-2 px-4 my-2 text-white uppercase cursor-pointer' onClick={(e) => handleSubmit(e)} >{!status.submitting ? !status.submitted ? 'Submit message' : 'Submitted' : 'Submitting...'}
+          </button>
+        </form>
       </article>
     </section>
   )
