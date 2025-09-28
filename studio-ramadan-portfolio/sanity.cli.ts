b7550f4f1ee7b2ce7c769ffe6853e1b63512fcb8
chrispydev <christianowusu44@gmail.com>
2025-09-28 17:11:20 +0000

    feat(file name): sanity api config token

--- studio-ramadan-portfolio/sanity.cli.ts
+++ studio-ramadan-portfolio/sanity.cli.ts
@@ -3,8 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4qf89tvc',
-    dataset: 'production'
+    dataset: 'production',
   },
+  deployment: {
+    appId: 'cxfa2k7ob96q2c4t5w6trxzc',
+  },
+
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
